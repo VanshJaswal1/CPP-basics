7e13585f8019a19c8a4ddd2e2b10d315cf327640
VanshJaswal1 <vanshjaswal17@email.com>
2026-05-29 22:14:29 +0530

    Added beginner C++ programs

---
+++ common.cpp
@@ -0,0 +1,18 @@
+//  THESE ARE  BASIC CODE YOU MUST KNOW WHILE LEARNING THE CPP
+
+#include <iostream>
+using namespace std;
+int main()
+ {
+    string name;
+  cout<<" enter your name ";
+  cin>> name;
+  cout<<" your  name is :" << name;
+ // when you write vansh jaswal it will only show vansh as it reads only vansh so to ivercome this situtaion we use use getline function
+
+//  getline(cin, name);
+ 
+ return 0; 
+}
+
+
---
+++ datatype.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+  cout<< " DIFFRENT TYPES OF DATA TYPES "<< endl;
+
+int a = 40;
+float b = 20.0f;
+char grade = 'v';
+string c = "vansh jaswal";
+ cout<< a<<endl;
+ cout<< b<<endl;
+ cout<< c << endl;
+ cout<< grade<< endl;
+
+
+}

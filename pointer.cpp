6fa229259083e77fb6e41d2374ea352245c3cb08
VanshJaswal1 <vanshjaswal17@gmail.com>
2026-06-24 12:14:29 +0530

    add them

---
+++ pointer.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+ cout<<"pointersss"<<endl;
+ int a= 22;
+ int*b;
+ b = &a;
+ cout<<"print the vakue of b "<<b<<endl;
+ cout<<"print the vakue of b "<<*b<<endl;
+
+
+
+
+
+}

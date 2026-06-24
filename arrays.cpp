340decc485ea5a21ce7760df9326881851397a30
VanshJaswal1 <vanshjaswal17@gmail.com>
2026-06-24 12:45:22 +0530

    addd themm

---
+++ arrays.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int arr[6] = { 2 , 3 , 4, 5, 6,7};
+    // printing a element from a array
+    cout<< arr[2]<<endl;
+ // printing a whole array
+ for( int i=0; i<6; i++) {
+  cout<< arr[i];
+ }
+}

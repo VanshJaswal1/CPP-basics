7662a24594c78467efb2bb2a3e803e264a537d0b
VanshJaswal1 <vanshjaswal17@gmail.com>
2026-06-24 12:18:06 +0530

    addd them

---
+++ pointer2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+ cout<<"pointersss"<<endl;
+ int a= 22;
+ int*b;
+ b = &a;
+int c = 78;
+
+ cout<<"basic sum code"<<endl;
+  cout<<"sum of c an b is "<< ( b + c )<<endl;
+  cout<<"sum of c an b is "<< ( *b + c )<<endl;
+
+
+
+
+}

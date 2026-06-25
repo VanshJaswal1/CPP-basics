338da61865016dea3361045eb34d4776c201bcbb
VanshJaswal1 <vanshjaswal17@gmail.com>
2026-06-25 12:06:17 +0530

    adddd themm

--- arrays.cpp
+++ arrays.cpp
@@ -10,5 +10,4 @@ int main() {
   cout<< arr[i]<<endl;
  }
   cout<< sizeof(arr)<<endl;
-
 }
--- arrays2.cpp
+++ arrays2.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 using namespace std;
 int main() {
-
-    int arr[6] = { 2 , 3 , 4, 5, 6,7};
+   // adding the diffrent numbers
+    int arr[5] = { 2 , 3 , 4, 5, 6};
     int Arr[7] = { 2 , 3 , 4, 5, 6,7,8};
-     cout<< arr[6] + arr[7];
-}
+     cout<< arr[3] + Arr[5]<<endl;
+ // adiing the all the elements 
+ int aRr[4] = {1,2,3,4};
+ int sum=0;
+ for( int i=0;i<4;i++) {
+    sum+=aRr[i];
+ }
+ cout<<sum<<endl;
+
+
+    }

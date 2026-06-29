d66fa8fc72678c14b2868c8d2f82aba306d91fec
VanshJaswal1 <vanshjaswal17@gmail.com>
2026-06-29 12:53:26 +0530

    addd themmm

---
+++ aandp.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[6] = {1,2,3,4,5,6};
+     int* ptr = arr;
+     cout<<arr<<endl;
+     cout<<&arr<<endl;
+        cout<<*(arr)<<endl;
+
+     return 0;
+}
